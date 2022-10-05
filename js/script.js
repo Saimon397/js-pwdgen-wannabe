3656f1fd9d1517b22439a0262dbc9216a7ac314d
Saimon397 <simonecarrabba@gmail.com>
2022-10-05 16:23:47 +0200

    mod_js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let UserFname = prompt("What's your name ?");
-let UserLname = prompt("What's your last name ?");
-let UserColor = prompt("What is your favorite color ?");
-let UserAge = prompt("How old are you ?");
+let UserFname = prompt("What's your name ?").trim().toLowerCase().replace(' ', '').replace('  ', '').replace('   ', '');
+let UserLname = prompt("What's your last name ?").trim().toLowerCase().replace(' ', '').replace('  ', '').replace('   ', '');
+let UserColor = prompt("What is your favorite color ?").trim().toLowerCase().replace(' ', '').replace('  ', '').replace('   ', '');
+let UserAge = prompt("How old are you ?").trim().toLowerCase().replace(' ', '').replace('  ', '').replace('   ', '');
 let UserPassword = UserFname + UserLname + UserColor + UserAge;
 document.getElementById("password").innerHTML = UserPassword;
